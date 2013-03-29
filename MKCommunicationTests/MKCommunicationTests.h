5033ce180b62a525c2f99edcb9640225747648e3
frank <frank@frankblumenberg.de>
2013-03-29 10:02:50 +0100

    Initial Commit

---
+++ MKCommunicationTests/MKCommunicationTests.h
@@ -0,0 +1,13 @@
+//
+//  MKCommunicationTests.h
+//  MKCommunicationTests
+//
+//  Created by Frank Blumenberg on 29.03.13.
+//  Copyright (c) 2013 Frank Blumenberg. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MKCommunicationTests : SenTestCase
+
+@end
---
+++ MKCommunicationTests/MKCommunicationTests.m
@@ -0,0 +1,32 @@
+//
+//  MKCommunicationTests.m
+//  MKCommunicationTests
+//
+//  Created by Frank Blumenberg on 29.03.13.
+//  Copyright (c) 2013 Frank Blumenberg. All rights reserved.
+//
+
+#import "MKCommunicationTests.h"
+
+@implementation MKCommunicationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MKCommunicationTests");
+}
+
+@end
